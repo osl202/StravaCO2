66d96e14e6da76d07075610d6a1f833b0b37fb56
Jack Morton <jjm229@exeter.ac.uk>
2024-01-11 02:31:04 +0000

    Strava oauth, basic sign in

--- README.md
+++ README.md
@@ -6,10 +6,12 @@
 - Start the server with `flask run` in the root directory, add the option `--debug` to enable hot refresh of file changes
 
 ## File structure
+- `stravaC02.py` is the entry point, run by flask and will start the server
 - `server/` contains the flask server
 	- `__init__.py` creates the flask app
 	- `routes.py` defines the endpoints available on the website, and sends the responses
+	- `oauth.py` contains functions to get the access keys from Strava
+	- `api.py` contains functions to fetch from the Strava API
 	- `templates/` contains the html templates which are put together by flask before sending as a response
 	- `static/` contains files that are served as-is, e.g. images, CSS styles, JavaScript
-- `stravaC02.py` is the entry point, run by flask and will start the server
-- `.flaskenv` tells flask which python script to run
+- `.flaskenv` tells flask which python script to start running
--- server/routes.py
+++ server/routes.py
@@ -1,10 +1,54 @@
 from . import app
-from flask import render_template
+from flask import render_template, request, Response, redirect
+import requests
+
+from . import oauth
+from . import api
 
 @app.route('/')
 def home():
-    return render_template('index.html', title='StravaC02')
+    refresh = request.cookies.get('refresh-token')
+    if not refresh:
+        # The user hasn't authorized
+        return render_template('home/index.html', title='StravaC02', connect_url=oauth.connect_url, auth=False)
+
+    tokens = oauth.OAuthTokens.from_refresh(refresh)
+    res = api.athlete(tokens.access)
+    print(str(res))
+    return render_template('profile/index.html', title='StravaC02', connect_url=oauth.connect_url, auth=True, **res)
 
 @app.route('/about')
 def about():
-    return render_template('about/index.html', title='StravaC02')
+    refresh = request.cookies.get('refresh-token')
+    return render_template('about/index.html', title='StravaC02', connect_url=oauth.connect_url, auth=bool(refresh))
+
+@app.route('/authorize')
+def authorize():
+    # We have an authorization request from Strava
+
+    error = request.args.get('error')
+    if error:
+        return error
+
+    # Get the access and refresh tokens
+    code = request.args.get('code')
+    tokens = oauth.OAuthTokens.from_code(code)
+
+    # Set the refresh token as a cookie on the client
+    res = redirect('/')
+    res.set_cookie('refresh-token', tokens.refresh, httponly=True, max_age=2592000)
+
+    return res
+
+@app.route('/deauthorize')
+def deauthorize():
+    refresh = request.cookies.get('refresh-token')
+    if not refresh:
+        # The user hasn't authorized
+        return render_template('home/index.html', title='StravaC02', connect_url=oauth.connect_url)
+
+    tokens = oauth.OAuthTokens.from_refresh(refresh)
+    tokens.deauthorize()
+    res = redirect('/')
+    res.delete_cookie('refresh-token')
+    return res
