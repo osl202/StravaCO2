c18ef94e1e5782afc4e531cbd0b836d7c747bf85
osl202 <osl202@exeter.ac.uk>
2024-01-13 13:41:45 +0000

    Added Carbon Interface API - to calculate equivalent CO2 emissions from car

--- carboninterface.py
+++ carboninterface.py
@@ -1,20 +1,32 @@
-
 import requests
 
-url = "https://www.carboninterface.com/api/v1/estimates"
-headers = {
-    "Authorization": "Bearer API_KEY",
-    "Content-Type": "application/json"
-}
+class Emissions:
 
-data = {
-      "type": "vehicle",
-      "distance_unit": "mi",
-      "distance_value": STRAVA_DISTANCE,
-      "vehicle_model_id": "7268a9b7-17e8-4c8d-acca-57059252afe9"   # Toyota Corolla  
-}
+    def __init__(self):
+        self.api_calls = 0
+        
+    def emissions(self, distance):
 
-response = requests.post(url, headers=headers, json=data)
+        url = "https://www.carboninterface.com/api/v1/estimates"
 
-print(response.status_code)
-print(response.json())
+        headers = {
+            "Authorization": "Bearer API_KEY",
+            "Content-Type": "application/json"
+        }
+
+        self.api_calls += 1
+        print(self.api_calls)
+        if self.api_calls > 50:
+            print("No more calls!")
+    
+        data = {
+        "type": "vehicle",
+        "distance_unit": "km",
+        "distance_value": distance,
+        "vehicle_model_id": "7268a9b7-17e8-4c8d-acca-57059252afe9"   # Toyota Corolla  
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+
+        return response.json()['data']['attributes']['carbon_kg']
+
